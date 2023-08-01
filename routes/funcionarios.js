0a062c7fba37a98b684e276e1a76b444542d8594
Joao Vitor <133013977+VitorAraujo98@users.noreply.github.com>
2023-08-01 19:02:35 -0300

    api-documentada

--- routes/funcionarios.js
+++ routes/funcionarios.js
@@ -4,50 +4,148 @@ const mysql = require('../mysql').pool;
 
 // RETORNA TODOS OS FUNCIONARIOS..
 router.get('/',(req, res, next) => {
-  res.status(200).send({
-    mensagem: 'Funcionários retornados na empresa..'
-  })
-});
+  mysql.getConnection((error, conn) => {
+  if(error) {return res.status(500).send({ error: error})}
+  conn.query(
+    'SELECT * FROM funcionarios;',
+    (error, results, field) => {
+      if(error) {return res.status(500).send({ error: error})}
+      const response = {
+        quantidade: results.length,
+        funcionario: results.map(func => {
+          return {
+            id_funcionario: func.id_funcionario,
+            nome: func.nome,
+            idade: func.idade,
+            preco: func.preco,
+            trabalho: func.trabalho,
+            endereco: func.endereco,
+            request: {
+            tipo: 'GET',
+            descricao: 'Retorna os detalhes de um funcionário específico',
+            url: 'http://localhost:3000/funcionarios/' + func.id_funcionario
+            }
+          }
+        })
+      }
+      return res.status(200).send({response})
+    }
+    )
+  });
+  
+  });
 
 
 router.get('/:id_funcionario',(req, res, next) => {
-    const id = req.params.id_funcionario
-    if (id === 'especial') {
-        res.status(200).send({
-            mensagem: 'Voce achou o funcionario especial'
-        })
-    } else {
-        res.status(200).send({
-            mensagem: 'Voce nao conseguiu localizar o funcionario especial',
-            id: id
-        })
-    }
+  mysql.getConnection((error, conn) => {
+    if(error) {return res.status(500).send({ error: error})}
+    conn.query(
+      'SELECT * FROM funcionarios WHERE id_funcionario = ?;',
+      [req.params.id_funcionario],
+      (error, results, field) => {
+        if(error) {return res.status(500).send({ error: error})}
+
+        if (results.length == 0) {
+          return res.status(404).send({
+            mensagem: 'Não foi encontrado o funcionário'
+          })
+        }
+        const response = {
+            funcionario: {
+            id_funcionario: results[0].id_funcionario,
+            nome: results[0].nome,
+            idade: results[0].idade,
+            preco: results[0].preco,
+            trabalho: results[0].trabalho,
+            endereco: results[0].endereco,
+            request: {
+              tipo: 'GET',
+              descricao: 'Retorna todos os funcionários',
+              url: 'http://localhost:3000/funcionarios'
+            }
+          }
+        }
+
+        return res.status(200).send({response: results})
+      }
+      )
+    });
 });
 
 
 // INSERE UM FUNCIONÁRIO NA EMPRESA..
 router.post('/',(req, res, next) => {
-  const funcionario = {
-    nome: req.body.nome,
-    idade: req.body.idade,
-    preco: req.body.preco,
-    trabalho: req.body.trabalho,
-    endereco: req.body.endereco
-  }
-
-    res.status(201).send({
-      mensagem: 'Funcionários inserido na empresa..',
-      funcionarioCriado: funcionario
-    })
+  mysql.getConnection((error, conn) => {
+    if(error) {return res.status(500).send({ error: error})}
+    conn.query(
+      'INSERT INTO funcionarios (nome, idade, preco, trabalho, endereco) VALUES(?,?,?,?,?)',
+      [req.body.nome, req.body.idade, req.body.preco, req.body.trabalho, req.body.endereco],
+      (error, results, field) => {
+        conn.release();
+        if(error) {return res.status(500).send({ error: error})}
+       const response = {
+        mensagem: 'Funcionário inserido com sucesso',
+        funcionarioCriado: {
+          id_funcionario: results.id_funcionario,
+          nome: req.body.nome,
+          idade: req.body.idade,
+          preco: req.body.preco,
+          trabalho: req.body.trabalho,
+          endereco: req.body.endereco,
+          request: {
+          tipo: 'GET',
+          descricao: 'Retorna todos os funcionários ',
+          url: 'http://localhost:3000/funcionarios'
+          }
+        }
+       }
+        return res.status(201).send(response);
+   
+         }
+       ) 
+    });
   });
 
 
 
 // ALTERA UM FUNCIONÁRIO NA 
   router.patch('/',(req, res, next) => {
-    res.status(200).send({
-      mensagem: 'Funcionários alterado na empresa..'
-    })
+    mysql.getConnection((error, conn) => {
+      if(error) {return res.status(500).send({ error: error})}
+      conn.query(
+        `UPDATE funcionarios 
+          SET    nome        = ?,
+                 idade       = ?,
+                 preco       = ?,
+                 trabalho    = ?,
+                 endereco    = ?
+        WHERE id_funcionario = ?`,
+        [req.body.nome, req.body.idade, req.body.preco, req.body.trabalho, req.body.endereco,
+         req.body.id_funcionario],
+        (error, results, field) => {
+          conn.release();
+          if(error) {return res.status(500).send({ error: error})}
+
+          const response = {
+            mensagem: 'Funcionário atualizado com sucesso',
+            funcionario: {
+              id_funcionario: req.body.id_funcionario,
+              nome: req.body.nome,
+              idade: req.body.idade,
+              preco: req.body.preco,
+              trabalho: req.body.trabalho,
+              endereco: req.body.endereco,
+              request: {
+                tipo: 'GET',
+                descricao: 'Retorna os detalhes da atualização do funcionário',
+                url: 'http://localhost:3000/funcionarios/' + req.body.id_funcionario
+                }
+            }
+           }
+            return res.status(202).send(response);
+           }
+         ) 
+      });
   });
 
 
@@ -55,9 +153,27 @@ router.post('/',(req, res, next) => {
 
  // REMOVE UM FUNCIONARIO
   router.delete('/',(req, res, next) => {
-    res.status(200).send({
-      mensagem: 'Funcionários retornados na empresa..'
-    })
+    mysql.getConnection((error, conn) => {
+      if(error) {return res.status(500).send({ error: error})}
+      conn.query(
+        `DELETE FROM funcionarios WHERE id_funcionario = ?`, [req.body.id_funcionario],
+        (error, results, field) => {
+          conn.release();
+          if(error) {return res.status(500).send({ error: error})}
+
+           const response = {
+             mensagem: 'Funcionário removido com sucesso',
+             request: {
+              tipo: 'POST',
+              descricao: 'Insere um funcionário',
+              url: 'http://localhost:3000/funcionarios',
+             }
+           }
+          return res.status(202).send(response);
+     
+           }
+         ) 
+      });
   });
   
 
--- routes/pedidos.js
+++ routes/pedidos.js
@@ -1,48 +1,141 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('../mysql').pool;
 
 // RETORNA TODOS OS PEDIDOS..
 router.get('/',(req, res, next) => {
-  res.status(200).send({
-    mensagem: 'Pedidos retornados com sucesso.'
-  })
+  mysql.getConnection((error, conn) => {
+    if(error) {return res.status(500).send({ error: error})}
+    conn.query(
+      'SELECT * FROM pedidos;',
+      (error, results, field) => {
+        if(error) {return res.status(500).send({ error: error})}
+        const response = {
+          quantidade: results.length,
+          pedidos: results.map(pedido => {
+            return {
+              id_pedido: pedido.id_pedido,
+              quantidade: pedido.quantidade,
+              id_funcionario: pedido.id_funcionario,
+              nota: pedido.nota,
+              request: {
+              tipo: 'GET',
+              descricao: 'Retorna os detalhes de um pedido específico',
+              url: 'http://localhost:3000/pedidos/' + pedido.id_pedido
+              }
+            }
+          })
+        }
+        return res.status(200).send({response})
+      }
+      )
+    });
 });
 
 
 router.get('/:id_pedido',(req, res, next) => {
-    const id = req.params.id_funcionario
-    if (id === 'especial') {
-        res.status(200).send({
-            mensagem: 'Voce achou o pedido especial'
-        })
-    } else {
-        res.status(200).send({
-            mensagem: 'Voce nao conseguiu localizar o pedido especial',
-            id: id
-        })
-    }
+  mysql.getConnection((error, conn) => {
+    if(error) {return res.status(500).send({ error: error})}
+    conn.query(
+      'SELECT * FROM pedidos WHERE id_pedido = ?;',
+      [req.params.id_pedido],
+      (error, results, field) => {
+        if(error) {return res.status(500).send({ error: error})}
+        
+        if (results.length == 0) {
+          return res.status(404).send({
+            mensagem: 'Não foi encontrado o pedido'
+          })
+        }
+        const response = {
+            pedido: {
+            id_pedido: results[0].id_pedido,
+            quantidade: results[0].quantidade,
+            id_funcionario: results[0].id_funcionario,
+            nota: results[0].nota,
+            request: {
+              tipo: 'GET',
+              descricao: 'Retorna todos os pedidos',
+              url: 'http://localhost:3000/pedidos'
+            }
+          }
+        }
+
+        return res.status(200).send({response})
+      }
+      )
+    });
 });
 
 
 // INSERE UM PEDIDO NA EMPRESA..
 router.post('/',(req, res, next) => {
-  const pedido = {
-    id_funcionario: req.body.id_funcionario,
-    quantidade: req.body.quantidade,
-    nota: req.body.nota
-  }
-    res.status(201).send({
-      mensagem: 'Pedido inserido com sucesso.',
-      pedidoCriado: pedido
-    })
+  mysql.getConnection((error, conn) => {
+      if(error) {return res.status(500).send({ error: error})}
+  conn.query('SELECT * FROM funcionarios WHERE id_funcionario = ?', 
+  [req.body.id_funcionario],
+  (error, results, field) => { 
+      if(error) {return res.status(500).send({ error: error})} 
+
+      if (results.length == 0) {
+        return res.status(404).send({
+          mensagem: 'Produto nao encontrado'
+        })
+      }
+      conn.query(
+        'INSERT INTO pedidos (id_pedido, quantidade, id_funcionario, nota) VALUES(?,?,?,?)',
+        [req.body.id_pedido, req.body.quantidade, req.body.id_funcionario, req.body.nota],
+        (error, results, field) => {
+          conn.release();
+          if(error) {return res.status(500).send({ error: error})}
+         const response = {
+          mensagem: 'Pedido inserido com sucesso',
+         pedidoCriado: {
+             id_pedido: results.id_pedido,
+             quantidade: req.body.quantidade,
+             id_funcionario: req.body.id_funcionario,
+             nota: req.body.nota,
+             request: {
+             tipo: 'GET',
+             descricao: 'Retorna todos os pedidos ',
+             url: 'http://localhost:3000/pedidos'
+            }
+          }
+         }
+          return res.status(201).send(response);
+           }
+         ) 
+      });
+      
+
+    });
   });
+
 
 
  // REMOVE UM PEDIDO
   router.delete('/',(req, res, next) => {
-    res.status(200).send({
-      mensagem: 'Pedido removido..'
-    })
+    mysql.getConnection((error, conn) => {
+      if(error) {return res.status(500).send({ error: error})}
+      conn.query(
+        `DELETE FROM pedidos WHERE id_pedido = ?`, [req.body.id_pedido],
+        (error, results, field) => {
+          conn.release();
+          if(error) {return res.status(500).send({ error: error})}
+
+           const response = {
+             mensagem: 'Pedido removido com sucesso',
+             request: {
+              tipo: 'POST',
+              descricao: 'Insere um Pedido',
+              url: 'http://localhost:3000/pedidos',
+             }
+           }
+          return res.status(202).send(response);
+     
+           }
+         ) 
+      });
   });
   
 
